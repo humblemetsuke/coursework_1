4645092b68b8a4aa35bd51274186409efd35541a
username <email>
2019-11-04 20:15:47 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,11 +6,11 @@ for (int i = 0; i < 21; i++) {
   System.out.println(i);
 }
 
-for (int j = 0; j < 21; i++) {
+for (int j = 0; j < 21; j++) {
   System.out.println(j);
 }
 
-for (int k = 0; k < 21; i++) {
+for (int k = 0; k < 21; k++) {
   System.out.println(k);
 }
 
