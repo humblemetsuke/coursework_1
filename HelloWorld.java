899ac1df0331381c15ff0ba5012273ac28e48704
username <email>
2019-11-04 18:33:55 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,28 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
        
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
-
+for (int i = 0; i < 21; i++) {
+  System.out.println(i);
+}
 //Prints Hello World 20 times. 
 
  
