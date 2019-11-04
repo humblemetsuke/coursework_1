11b763ec9a43ec1d919f77a1087e224c4a30b4a8
username <email>
2019-11-04 18:28:58 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,85 +1,33 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+       
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
 
-        System.out.println("Hello, World");
+ 
 
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
-
-        System.out.println("Hello, World");
+        
+      
 
     }
 }
