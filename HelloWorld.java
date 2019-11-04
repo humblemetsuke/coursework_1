3a71f7b87b1ff6cbe1cc328f4b9d1eb9b416fd0d
username <email>
2019-11-04 20:47:45 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,23 +1,40 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-       
-for (int i = 0; i < 21; i++) {
-  System.out.println(i);
-}
+import java.util.Scanner;
 
-for (int j = 0; j < 21; j++) {
-  System.out.println(j);
-}
+public class HelloWorld
+{
 
-for (int k = 0; k < 21; k++) {
-  System.out.println(k);
-}
+    public static int Arg1;
+  
 
- 
+    public static void main(String args[])
+    {
+        if (args.length > 0) 
+        {
+            try 
+            {
+                Arg1 = Integer.parseInt(args[0]);
+            } 
+            catch (NumberFormatException e) 
+            {
+                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.exit(1);
+            }
+        }
 
+    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+         int rem, num;
+	num = Arg1;
+	String hexadecimal=""; 
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
-      
+        while(num != 0)
+        {
+	    rem=num%16;
+	    hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
+        }
+        
+        System.out.println("Hexadecimal representation is : " + hexadecimal);
 
     }
 }
