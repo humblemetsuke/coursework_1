3457c9c9fecc8f650f41a267e8c49bceba3c57df
username <email>
2019-11-05 17:20:02 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 public class Dec2Hex
 {
     public static int Arg1;
-Scanner scan = new Scanner(System.in);
-System.out.print("Enter any number: ");
-Arg1=scan.nextInt();
-scan.close();
+
 
 
     public static void main(String args[])
     {
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter any number: ");
+Arg1= scan.nextInt();
+scan.close();
         if (args.length > 0) 
         {
             try 
