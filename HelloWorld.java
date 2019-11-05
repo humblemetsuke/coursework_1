9fb860dc7d86dba0ec5112daa59ed3d17fbdd482
username <email>
2019-11-05 11:59:42 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,40 +1,49 @@
-import java.util.Scanner;
 
-public class HelloWorld
+public class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+    String x=    "Hello World!";
+
+for (int i=0;i<10;i++)
 {
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      for (int i=0;i<10;i++)
+{
+System.out.println(x);
+}
+      
 
-    public static int Arg1;
-  
-
-    public static void main(String args[])
-    {
-        if (args.length > 0) 
-        {
-            try 
-            {
-                Arg1 = Integer.parseInt(args[0]);
-            } 
-            catch (NumberFormatException e) 
-            {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
-            }
-        }
-
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-
-    }
+  }
 }
