b3c475a2e710c594810286cf212e2ca2e7704118
username <email>
2019-11-07 10:14:31 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,25 +1,31 @@
-//import java.util.Scanner;
+package com.example;
+
+
+
 public class Dec2Hex
 {
 public static int Arg1;
 
 public static void main(String args[])
 {
-//Scanner scan = new Scanner(System.in);
-//System.out.println("Enter any number: ");
-//Arg1= scan.nextInt();
-//scan.close();
+
+
 if (args.length == 0)
 {
 try
 {
 Arg1 = Integer.parseInt(args[0]);
 }
-catch (NumberFormatException e)
+  
+catch (ArrayIndexOutOfBoundsException e)
+{
+System.err.println("No Argument Provided.");
+System.exit(1);
+}
+  catch (NumberFormatException e)
 {
 System.err.println("Argument" + args[0] + " must be an integer.");
 System.exit(1);
-}
 }
 char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 int rem;
@@ -36,5 +42,6 @@ num= num/16;
 }
 
 System.out.println("Hexadecimal representation is : " + hexadecimal);
+}
 }
 }
