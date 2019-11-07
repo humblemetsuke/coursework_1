6ed1220e88621ea2f72b08c93ad24ab18956dbd5
username <email>
2019-11-07 09:27:08 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,42 +1,40 @@
 import java.util.Scanner;
 public class Dec2Hex
 {
-    public static int Arg1;
+public static int Arg1;
 
-
-
-    public static void main(String args[])
-    {
+public static void main(String args[])
+{
 Scanner scan = new Scanner(System.in);
 System.out.println("Enter any number: ");
 Arg1= scan.nextInt();
 scan.close();
-        if (args.length > 0) 
-        {
-            try 
-            {
-                Arg1 = Integer.parseInt(args[0]);
-            } 
-            catch (NumberFormatException e) 
-            {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
-            }
-        }
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem; 
-      int num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-    }
+if (args.length > 0)
+{
+try
+{
+Arg1 = Integer.parseInt(args[0]);
+}
+catch (NumberFormatException e)
+{
+System.err.println("Argument" + args[0] + " must be an integer.");
+System.exit(1);
+}
+}
+char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+int rem;
+int num;
+num = Arg1;
+String hexadecimal="";
+System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+while(num != 0)
+{
+rem=num%16;
+hexadecimal= ch[rem] + hexadecimal;
+num= num/16;
+}
+
+System.out.println("Hexadecimal representation is : " + hexadecimal);
+}
 }
